dcac5182fb5429dcb0e76dc4553ddb677b7d4e5a
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-08-17 14:16:40 +0300

    Reproduced the code from manual

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,37 @@ namespace Asteroids
 
     internal sealed class Player : MonoBehaviour
     {
+        [SerializeField] private float _speed;
+        [SerializeField] private float _hp;
+        [SerializeField] private float _force;
+        [SerializeField] private Rigidbody2D _bullet;
+        [SerializeField] private Transform _barrel;
+        private Vector3 _move;
 
+        private void Update()
+        {
+            var deltatime = Time.deltaTime;
+            var speed = _speed * deltatime;
+            _move.Set(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed, 0.0f);
+            transform.localPosition += _move;
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                var temAmmution = Instantiate(_bullet, _barrel.position, _barrel.rotation);
+                temAmmution.AddForce(_barrel.up * _force);
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if(_hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                _hp--;
+            }
+        }
     }
 }
