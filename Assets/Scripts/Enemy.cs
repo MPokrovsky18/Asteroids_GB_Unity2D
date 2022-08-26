6b3a33e16b17519154990c94f1f2fd8482e109cc
Maxim Pokrovsky <maxim.pokrovsky@outlook.com>
2022-08-26 22:33:58 +0300

    Implemented object pool from manual

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using Asteroids.Object_Pool;
 using UnityEngine;
 
 
@@ -7,7 +8,36 @@ namespace Asteroids
     internal abstract class Enemy : MonoBehaviour
     {
         public static IEnemyFactory Factory;
-        public Health Health { get; private set; }
+        private Transform _rootPool;
+        private Health _health;
+
+        public Health Health
+        {
+            get
+            {
+                if(_health.Current <= 0.0f)
+                {
+                    ReturnToPool();
+                }
+
+                return _health;
+            }
+            protected set => _health = value;
+        }
+
+        public Transform RootPool
+        {
+            get
+            {
+                if(_rootPool == null)
+                {
+                    var find = GameObject.Find(NameManager.POOL_AMMUNITION);
+                    _rootPool = find == null ? null : find.transform;
+                }
+
+                return _rootPool;
+            }
+        }
 
         public static Asteroid CreateAsteroidEnemy(Health hp)
         {
@@ -19,6 +49,27 @@ namespace Asteroids
         public void DependencyInjectHealth(Health hp)
         {
             Health = hp;
+        }
+
+        private void ActiveEnemy(Vector3 position, Quaternion rotation)
+        {
+            transform.localPosition = position;
+            transform.localRotation = rotation;
+            gameObject.SetActive(true);
+            transform.SetParent(null);
+        }
+
+        private void ReturnToPool()
+        {
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+            gameObject.SetActive(false);
+            transform.SetParent(_rootPool);
+
+            if (!_rootPool)
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
---
+++ Assets/Scripts/NameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+namespace Asteroids.Object_Pool
+{
+
+    internal static class NameManager
+    {
+        public const string POOL_AMMUNITION = "[Pool_Ammunition]";
+    }
+}
